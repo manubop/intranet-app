4a4d9dbaabc140bf651a577daeafd95cb0f9a583
manubop <manubop@gmail.com>
2021-03-28 20:00:00 +0200

    Added path to static files & listen on 127.0.0.1

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.set('trust proxy', true);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(verify());
 
 app.use(require('./controllers'));
 
-app.listen(3000, function () {
+app.listen(3000, '127.0.0.1', function () {
     console.log('Example app listening on port 3000!');
 });
