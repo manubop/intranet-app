a3e7652d92485641e9f9e23e5e0d1455a0ed092d
manubop <manubop@gmail.com>
2021-04-10 19:16:15 +0200

    Use path lib to join elements

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const verify = require('./middlewares/verify');
+const path = require('path');
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.set('trust proxy', true);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(verify());
 
 app.use(require('./controllers'));
